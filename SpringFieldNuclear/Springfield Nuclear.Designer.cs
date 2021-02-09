5bd3782254bdbc641bebcb34f9eaefde607be129
Bilal Zeineddine <bilazein302@ed.amdsb.ca>
2021-02-09 09:35:57 -0500

    Attempt #2

---
+++ SpringFieldNuclear/Springfield Nuclear.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace SpringFieldNuclear
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "NuclearPowerControlLabel";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Zilla Slab Medium", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(142, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nuclear Power Control";
+            // 
+            // label2
+            // 
+            this.label2.AccessibleName = "Reactor1Label";
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Zilla Slab Medium", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label2.Location = new System.Drawing.Point(60, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reactor 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AccessibleName = "Reactor2Label";
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Font = new System.Drawing.Font("Zilla Slab Medium", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label3.Location = new System.Drawing.Point(371, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Reactor 2";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "Reactor1StateLabel";
+            this.label4.BackColor = System.Drawing.Color.LawnGreen;
+            this.label4.Location = new System.Drawing.Point(41, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 107);
+            this.label4.TabIndex = 3;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AccessibleName = "Reactor2StateLabel";
+            this.label5.BackColor = System.Drawing.Color.LawnGreen;
+            this.label5.Location = new System.Drawing.Point(354, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 107);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AccessibleName = "ReactorsStableLabel";
+            this.label6.BackColor = System.Drawing.SystemColors.Info;
+            this.label6.Font = new System.Drawing.Font("Zilla Slab", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(120, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(257, 40);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Reactors Stable";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.AccessibleName = "StartButton";
+            this.StartButton.Location = new System.Drawing.Point(212, 224);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(495, 313);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SpringfieldPowerPlant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ SpringFieldNuclear/Springfield Nuclear.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;//allows the use of Thread.Sleep()
+using System.Media;//allows the use of SoundPlayer
+
+namespace SpringFieldNuclear
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create a sound player and load the alert.wav sound, then play it 
+
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.alert);
+
+            alertPlayer.Play();
+            label4.BackColor = Color.Red;
+            label5.BackColor = Color.Red;
+            label6.Text = "Meltdown Imminent!";
+            label6.ForeColor = Color.Red;
+            label6.BackColor = Color.White;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.White;
+            label5.BackColor = Color.White;
+            label6.ForeColor = Color.White;
+            label6.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.Red;
+            label5.BackColor = Color.Red;
+            label6.ForeColor = Color.Red;
+            label6.BackColor = Color.White;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.White;
+            label5.BackColor = Color.White;
+            label6.ForeColor = Color.White;
+            label6.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.Red;
+            label5.BackColor = Color.Red;
+            label6.ForeColor = Color.Red;
+            label6.BackColor = Color.White;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.White;
+            label5.BackColor = Color.White;
+            label6.ForeColor = Color.White;
+            label6.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.Red;
+            label5.BackColor = Color.Red;
+            label6.ForeColor = Color.Red;
+            label6.BackColor = Color.White;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
